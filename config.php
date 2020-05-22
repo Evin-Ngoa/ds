295a97826ed2d8329a7fbbdfc0ad87f73b623110
Evin <toneevin07@gmail.com>
2020-05-22 23:23:09 +0300

    Changed the variables name

--- config.php
+++ config.php
@@ -1,21 +1,21 @@
 <?php
-$ini = parse_ini_file('config.ini');
+$ini_config = parse_ini_file('config.ini');
 return [
     'mysql' => [
         'driver' => 'mysql',
-        'host' => $ini['DB_MYSQL_SERVER'],
-        'port' => $ini['DB_MYSQL_PORT'],
-        'database' => $ini['DB_MYSQL_NAME'],
-        'username' => $ini['DB_MYSQL_USER'],
-        'password' => $ini['DB_MYSQL_PASSWORD'],
+        'host' => $ini_config['DB_MYSQL_SERVER'],
+        'port' => $ini_config['DB_MYSQL_PORT'],
+        'database' => $ini_config['DB_MYSQL_NAME'],
+        'username' => $ini_config['DB_MYSQL_USER'],
+        'password' => $ini_config['DB_MYSQL_PASSWORD'],
     ],
 
     'pgsql' => [
         'driver' => 'pgsql',
-        'host' => $ini['DB_POSTGRES_SERVER'],
-        'port' => $ini['DB_POSTGRES_PORT'],
-        'database' => $ini['DB_POSTGRES_NAME'],
-        'username' => $ini['DB_POSTGRES_USER'],
-        'password' => $ini['DB_POSTGRES_PASSWORD'],
+        'host' => $ini_config['DB_POSTGRES_SERVER'],
+        'port' => $ini_config['DB_POSTGRES_PORT'],
+        'database' => $ini_config['DB_POSTGRES_NAME'],
+        'username' => $ini_config['DB_POSTGRES_USER'],
+        'password' => $ini_config['DB_POSTGRES_PASSWORD'],
     ]
 ];
