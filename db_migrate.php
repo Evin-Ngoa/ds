3e4c628bb8b11272271de252b489c420e8b56805
Evin <toneevin07@gmail.com>
2020-05-24 00:46:13 +0300

    Added fragment creation

--- db_migrate.php
+++ db_migrate.php
@@ -123,3 +123,98 @@ function createPsqlF1F2F3F4Table($database)
 
     return $status;
 }
+
+
+
+/**
+ * Create Fragment 5 & 6 Tables
+ * in Mysql db
+ */
+function createMysqlCountriesTable($database)
+{
+    // Mysql Connection
+    $host = $database['mysql']['host'];
+    $user = $database['mysql']['username'];
+    $pass = $database['mysql']['password'];
+    $dbname = $database['mysql']['database'];
+    $status = 0;
+
+    $conn = mysqli_connect($host, $user, $pass, $dbname);
+
+    // Check connection
+    if ($conn->connect_error) {
+        $status = 0;
+        die("Connection failed in creating Mysql: " . $conn->connect_error);
+    }
+
+    $sql_countries = "
+     CREATE TABLE sql_countries 
+        (cid INT AUTO_INCREMENT,
+        region VARCHAR(20) NOT NULL,
+        country_name VARCHAR(20) NOT NULL,                
+        covid_cases INT NOT NULL,
+        male INT NOT NULL,
+        female INT NOT NULL,
+        primary key (cid));
+        ";
+
+    if (
+        mysqli_query($conn, $sql_countries)        
+    ) {
+        $status = 1;
+        echo "\n Table countries created successfully \n";
+    } else {
+        $status = 0;
+        echo "\n Table countries not created! \n";
+    }
+    mysqli_close($conn);
+
+    return $status;
+}
+
+/**
+ * Create Fragment 1,2,3 & 4
+ * in Postgres db
+ */
+function createPsqlCountriesTable($database)
+{
+    // Postgress Connection
+    $psqlHost = "host =". $database['pgsql']['host'];
+    $psqlPort = "port = ". $database['pgsql']['port'];
+    $psqlDbname = "dbname = ". $database['pgsql']['database'];
+    $credentials = "user = ". $database['pgsql']['username'] ." password=". $database['pgsql']['password'];
+    $status = 0;
+
+    $db = pg_connect( "$psqlHost $psqlPort $psqlDbname $credentials");
+
+    if (!$db) {
+        $status = 0;
+        echo "Error : Unable to open database in seeding Postgres\n";
+    } else {
+        $status = 1;
+        echo "Opened database successfully\n";
+    }
+    
+       $psql = <<<EOF
+       CREATE TABLE PSQL_COUNTRIES 
+       (CID INT PRIMARY KEY NOT NULL,
+       REGION TEXT NOT NULL,
+       COUNTRY_NAME TEXT NOT NULL,
+       COVID_CASES INT NOT NULL,
+       MALE INT NOT NULL,
+       FEMALE INT NOT NULL);
+      EOF;
+ 
+   
+    $ret = pg_query($db, $psql);    
+    if(!$ret)  {
+        $status = 0;
+       echo pg_last_error($db);
+    } else {
+        $status = 1;
+       echo "Tables created successfully\n";
+    }
+    pg_close($db);
+
+    return $status;
+}
